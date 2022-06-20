5d8dc59af3b2525204d9a698cefd2ec8860ec5ac
Bijirgiri Basumatary <75986148+bijirgiriiitg@users.noreply.github.com>
2022-06-20 15:19:10 +0530

    Add files via upload

---
+++ prd4agri/agrivision/src/sliders.js
@@ -0,0 +1,11 @@
+import ImageSlider from './Slider/Slider';
+import "./Slider/css/App.css";
+function App() {
+  return (
+    <div>
+        <ImageSlider/>
+    </div>
+  )
+}
+
+export default App;
